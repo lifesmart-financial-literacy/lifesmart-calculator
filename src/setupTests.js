a1de7e7eb547188cf65eb65b61f5ae0e7db2bb11
Humza Butt <Humzab1711@hotmail.com>
2025-12-10 14:45:38 +0000

    feat: mock window.matchMedia in setupTests.js for media query support
    - Added a mock implementation of window.matchMedia to facilitate testing of components that utilize media queries, ensuring consistent behavior during tests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Mock window.matchMedia for components that use media queries
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
